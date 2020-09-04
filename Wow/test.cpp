6e2700cc144bef8e25e8d4b37c34c28ff4eea272
Jacob Julien <2054060@cstj.qc.ca>
2020-09-04 13:19:04 -0700

    Premier Commit/Validation du projet avec Git

---
+++ Wow/test.cpp
@@ -0,0 +1,16 @@
+// But: Créer un programme qui affiche un message en sauvgardant plusieurs version de ce programme avec git et l'héberger sur la plateform GitHub
+//
+//
+// Auteur: Jacob Julien
+// Date : 2020/09/04
+
+int main( /*Liste des paramètres séparés par une virgule le cas échéant*/)
+{
+	
+	
+
+	
+	
+	
+	return 0;
+}
